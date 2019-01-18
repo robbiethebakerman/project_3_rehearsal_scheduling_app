8efeba687e43316a7dca603af9876222cd480e92
Alex McIntosh <nz.a.mcintosh@gmail.com>
2019-01-18 14:13:31 +0000

    initial commit

---
+++ Backend/rehearsalschedule/src/main/java/com/codelcan/example/rehearsalschedule/RehearsalscheduleApplication.java
@@ -0,0 +1,14 @@
+package com.codelcan.example.rehearsalschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehearsalscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehearsalscheduleApplication.class, args);
+	}
+
+}
+
