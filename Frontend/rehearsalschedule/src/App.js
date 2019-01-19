b854b84091d0e3b46a2e333b4f4b0ed94bb2b61e
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-19 15:18:37 +0000

    Sets up app.js with route structure, adds CalendarContainer

--- Frontend/rehearsalschedule/src/App.js
+++ Frontend/rehearsalschedule/src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import CalendarContainer from './containers/calendar/CalendarContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+            <Fragment>
+                {/*Put any elements we want on all pages BEFORE or AFTER the Switch element below,
+                e.g., Nav Bar put above it*/}
+                <Switch>
+                    {/*put all separate frontend routes within this Switch element
+                    if using a url with a parameter (e.g., id), then remember to use the "render" props, e.g.,
+                        <Route exact path="/pirates/:id" render={(props) => {
+                            const id = props.match.params.id;
+                            return <SinglePirateContainer id={id}/>
+                      }}/>*/}
+                    <Route exact path="/calendar" component={CalendarContainer}/>
+                </Switch>
+            </Fragment>
+        </Router>
       </div>
     );
   }
