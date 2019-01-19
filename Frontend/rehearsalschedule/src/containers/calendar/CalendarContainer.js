10765645854d029477ea503ad710de3693431002
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-19 16:11:09 +0000

    Sets up basic calendar view with dummy data

--- Frontend/rehearsalschedule/src/containers/calendar/CalendarContainer.js
+++ Frontend/rehearsalschedule/src/containers/calendar/CalendarContainer.js
@@ -2,15 +2,28 @@ import React, {Component, Fragment} from 'react';
 import BigCalendar from 'react-big-calendar';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.less'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import '../../css/calendar.css';
+import moment from "moment";
+import events from './rehearsalsDummyData';
+
+const localizer = BigCalendar.momentLocalizer(moment);
 
 class CalendarContainer extends Component {
 
 
     render() {
         return(
-            <Fragment>
+            <div className="calendar-container">
                 <h1>Calendar Container</h1>
-            </Fragment>
+                <BigCalendar
+                    localizer={localizer}
+                    events={events}
+                    startAccessor="start"
+                    endAccessor="end"
+                    titleAccessor="location"
+                />
+            </div>
         )
     }
 }
