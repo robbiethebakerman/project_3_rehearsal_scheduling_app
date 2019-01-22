bb183c0a9ae954718a5cda15a20950a99e89a9e2
Steven McAndrew <steven.mcandrew@outlook.com>
2019-01-22 17:30:54 +0000

    adds custom query to get allrehearsalsforproject

--- Backend/rehearsalschedule/src/main/java/com/codeclan/example/rehearsalschedule/controllers/ProjectController.java
+++ Backend/rehearsalschedule/src/main/java/com/codeclan/example/rehearsalschedule/controllers/ProjectController.java
@@ -1,9 +1,14 @@
 package com.codeclan.example.rehearsalschedule.controllers;
 
+import com.codeclan.example.rehearsalschedule.models.Rehearsal;
 import com.codeclan.example.rehearsalschedule.repositories.projectRepositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/projects")
@@ -11,5 +16,11 @@ public class ProjectController {
 
     @Autowired
     ProjectRepository projectRepository;
+
+
+    @GetMapping(value="/calendar/project/{id}/rehearsals")
+    public List<Rehearsal> allRehearsalsForProject(@PathVariable Long id){
+        return projectRepository.allRehearsalsForProject(id);
+    }
 
 }
--- Backend/rehearsalschedule/src/main/java/com/codeclan/example/rehearsalschedule/repositories/projectRepositories/ProjectRepository.java
+++ Backend/rehearsalschedule/src/main/java/com/codeclan/example/rehearsalschedule/repositories/projectRepositories/ProjectRepository.java
@@ -1,10 +1,14 @@
 package com.codeclan.example.rehearsalschedule.repositories.projectRepositories;
 
 import com.codeclan.example.rehearsalschedule.models.Project;
+import com.codeclan.example.rehearsalschedule.models.Rehearsal;
 import com.codeclan.example.rehearsalschedule.projections.EmbedForProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(excerptProjection = EmbedForProject.class)
 public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom{
+    List<Rehearsal> allRehearsalsForProject(Long id);
 }
