3c294efbac75e2739ef3b1547408303dacfbfd15
Steven McAndrew <steven.mcandrew@outlook.com>
2019-01-22 13:18:13 +0000

    fixed date input

--- Frontend/rehearsalschedule/src/components/formComponents/NewProjectForm.js
+++ Frontend/rehearsalschedule/src/components/formComponents/NewProjectForm.js
@@ -19,9 +19,9 @@ const NewProjectForm = (props) =>{
     <form onSubmit={handleSubmit}>
     <input type="text" placeholder="Name" name="name" />
     <label for="startDate">Choose start date:</label>
-    <input type="datetime-local" id="startDate" name="startDate" />
+    <input type="date" id="startDate" name="startDate" />
     <label for="endDate">Choose end date:</label>
-    <input type="datetime-local" id="endDate" name="endDate" />
+    <input type="date" id="endDate" name="endDate" />
     <button type="submit">Save</button>
     </form>
     </div>
