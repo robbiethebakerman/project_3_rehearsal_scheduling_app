9e6f68cc079af8f94fee9abc10912a95d11ae421
Steven McAndrew <steven.mcandrew@outlook.com>
2019-01-23 13:58:50 +0000

    adds a delete message component to member components

---
+++ Frontend/rehearsalschedule/src/components/memberComponents/MemberDeleteWarningMessage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MemberDeleteWarningMessage = (props) => {
+
+  const handleDelete = function(){
+    props.handleDeleteConfirm(props.member.id);
+  };
+
+  const handleReject = function(){
+    props.handleDeleteReject();
+  };
+
+  return (
+    <div>
+      <p> I'm a warning message</p>
+      <button onClick={handleDelete}>Yes, Delete</button>
+      <button onClick={handleReject}>No, don't delete!</button>
+      </div>
+  )
+};
+
+export default MemberDeleteWarningMEssage;
